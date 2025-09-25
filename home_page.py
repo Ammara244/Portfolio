69a39194ba7b848b4052baced64938cedf312e85
Ammara244 <ammarahajat24@gmail.com>
2025-09-25 13:25:03 +0100

    Create home_page.py

---
+++ home_page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Ammara Hajat')
+
+st.write('Welcome to my portfolio!👋')
+
+st.header('A bit about me:')
+st.write('I am currently as second year Mathematics and Artificial Intelligence student as the University of Leicester')
