fd4e4e870908450da7e4509bee25e62166e09c18
Ammara244 <ammarahajat24@gmail.com>
2025-10-08 11:05:02 +0100

    Update certificates.py

--- pages/certificates.py
+++ pages/certificates.py
@@ -4,8 +4,7 @@ st.title('My Certificates:')
 st.image('certificates/Intro_to_Programming_Kaggle.png')
 st.image('certificates/Python (Kaggle) - Ammara.png')
 st.image('certificates/Data_Visualization.png')
-st.image('Pandas (Kaggle) -Ammara.png')
-st.image('Python (Kaggle) - Ammara.png')
+st.image('certificates/Pandas (Kaggle) -Ammara.png')
 
 
 with open("certificates/Data_and_SQL_CFG.pdf", "rb") as file:
