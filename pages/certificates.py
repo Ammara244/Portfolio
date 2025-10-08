d87d25b9cfdc9b6b6251894250bc7d8120b0eeb0
Ammara244 <ammarahajat24@gmail.com>
2025-10-08 11:16:11 +0100

    Update certificates.py

--- pages/certificates.py
+++ pages/certificates.py
@@ -1,13 +1,27 @@
 import streamlit as st
 st.title('My Certificates:')
+st.write('Here are some extra certificates that I have gained:')
 
-with st.expander("Data Overview"):
+st.header('Kaggle Certificates')
+with st.expander("Introduction to Programming"):
     st.image('certificates/Intro_to_Programming_Kaggle.png')
 
-st.image('certificates/Python (Kaggle) - Ammara.png')
-st.image('certificates/Data_Visualization.png')
-st.image('certificates/Pandas (Kaggle) -Ammara.png')
+with st.expander('Python'):
+    st.image('certificates/Python (Kaggle) - Ammara.png')
 
+with st.expander('Data Visualistion'):
+    st.image('certificates/Data_Visualization.png')
+    
+with st.expander('Pandas (Python)'):
+    st.image('certificates/Pandas (Kaggle) -Ammara.png')
+
+st.header('Code First Girls Certificates')
+
+with open("certificates/Python (CFG)- Ammara Hajat.pdf", "rb") as file:
+    st.download_button("Download Introduction to Python Certificate", file, "Python (CFG)- Ammara Hajat.pdf")
 
 with open("certificates/Data_and_SQL_CFG.pdf", "rb") as file:
-    st.download_button("Download Certificate", file, "Data_and_SQL_CFG.pdf")
+    st.download_button("Download Data and SQL Certificate", file, "Data_and_SQL_CFG.pdf")
+
+with open("certificates/Javascript (CFG) - Ammara Hajat.pdf", "rb") as file:
+    st.download_button("Download Introduction to JavaScript Certificate", file, "Javascript (CFG) - Ammara Hajat.pdf")
