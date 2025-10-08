a5f99d88780591563a40c70b1fec85f30e0be456
Ammara244 <ammarahajat24@gmail.com>
2025-10-08 11:22:40 +0100

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -3,6 +3,8 @@ import streamlit as st
 st.title('Ammara Hajat')
 
 st.write('Welcome to my portfolio!👋')
+st.write('I am still in the process of making this website, so for now, please explore what I have so far!')
+st.markdown('This website was made using streamlit (python) on github.
 
-st.header('A bit about me:')
-st.write('I am currently as second year Mathematics and Artificial Intelligence student as the University of Leicester')
+st.subheader('A bit about me:')
+st.write('I am currently as second year Mathematics and Artificial Intelligence student as the University of Leicester, and my ambitions for the future is a career in tech.')
