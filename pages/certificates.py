fe48fcd20684d52471afb98d996ed7f796cbfcab
Ammara244 <ammarahajat24@gmail.com>
2025-10-08 10:41:53 +0100

    Update certificates.py

--- pages/certificates.py
+++ pages/certificates.py
@@ -1,11 +1,11 @@
 import streamlit as st
 st.title('My Certificates:')
 
+st.image('certificates/Intro to programming (Kaggle) - Ammara.png')
 st.image('certificates/Python (Kaggle) - Ammara.png')
 st.image('certificates/Data_Visualization.png')
-st.image('certificates/ ')
-st.image('certificates/ ')
-st.image('certificates/ ')
-st.image('certificates/ ')
-st.image('certificates/ ')
-st.image('certificates/ ')
+st.image('certificates/Data_and_SQL_CFG.pdf')
+st.image('certificates/Javascript (CFG) - Ammara Hajat.pdf')
+#st.image('certificates/ ')
+#st.image('certificates/ ')
+#st.image('certificates/ ')
