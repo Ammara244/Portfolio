1bf3133b7fbc762278fe07efa8a84211a971ca01
Ammara244 <ammarahajat24@gmail.com>
2025-10-08 10:38:30 +0100

    Update certificates.py

--- pages/certificates.py
+++ pages/certificates.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('My Certificates:')
 
-st.image('certificates/ ')
+st.image('certificates/Python (Kaggle) - Ammara.png')
 st.image('certificates/Data_Visualization.png')
 st.image('certificates/ ')
 st.image('certificates/ ')
