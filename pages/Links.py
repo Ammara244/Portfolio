f127cff6053133f0caef439a18fb3d249787ba06
Ammara <ammarahajat24@gmail.com>
2025-10-08 12:07:06 +0100

    Update and rename Links & Contact Info.py to Links.py

--- pages/Links.py
+++ pages/Links.py
@@ -3,7 +3,5 @@ import streamlit as st
 st.header('Links')
 
 st.markdown('[LinkedIn](www.linkedin.com/in/ammara-h-b57395338)')
-st.write('On my github profile, under Profile, you can find the code for this website')
 st.markdown('[GitHub](https://github.com/Ammara244)')
-
-st.header('Contact Me')
+st.write('On my github profile, under Profile, you can find the code for this website')
