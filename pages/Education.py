06cf37c617c0e54422346a02597612926abadc39
Ammara244 <ammarahajat24@gmail.com>
2025-10-08 11:47:38 +0100

    Update Education.py

--- pages/Education.py
+++ pages/Education.py
@@ -1,7 +1,10 @@
 import streamlit as st
 st.title('My Education')
 
-st.subheader('Current - BSc Mathematics and Artificial Intelligence (2024-2027)')
-st.write('**Current - BSc Mathematics and Artificial Intelligence (2024-2027)**')
-st.write('Modules:')
-Programming Fundementals, Probablity, Algorithms Data Structures and Advanced Programming, Introductory Statistics, Linear Algebra, Calculus and Analysis
+st.subheader('Current - BSc Mathematics and Artificial Intelligence - University of Leicester (2024-2027)')
+st.write('**Current - BSc Mathematics and Artificial Intelligence - University of Leicester (2024-2027)**')
+
+with st.expander('Modules:'):
+  st.write('Programming Fundementals, Probablity, Algorithms Data Structures and Advanced Programming, Introductory Statistics, Linear Algebra, Calculus and Analysis')
+
+st.subheader('A-Levels')
