edfc3d7a8c672b00f1c6bb607e1d68ed2b8dfaac
Ammara244 <ammarahajat24@gmail.com>
2025-10-08 11:23:17 +0100

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -4,7 +4,7 @@ st.title('Ammara Hajat')
 
 st.write('Welcome to my portfolio!👋')
 st.write('I am still in the process of making this website, so for now, please explore what I have so far!')
-st.markdown('This website was made using streamlit (python) on github.
+st.markdown('This website was made using streamlit (python) on github.')
 
 st.subheader('A bit about me:')
 st.write('I am currently as second year Mathematics and Artificial Intelligence student as the University of Leicester, and my ambitions for the future is a career in tech.')
