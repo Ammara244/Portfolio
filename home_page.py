57b519a2fd76ea5970a361685ba0628806061835
Ammara <ammarahajat24@gmail.com>
2025-10-16 10:22:26 +0100

    Fix typos and capitalization in home_page.py

--- home_page.py
+++ home_page.py
@@ -4,7 +4,7 @@ st.title('Ammara Hajat')
 
 st.write('Welcome to my portfolio!👋')
 st.write('I am still in the process of making this website, so for now, please explore what I have so far! Open the sidebar to find more.')
-st.markdown('This website was made using streamlit (python) on github.')
+st.markdown('This website was made using Streamlit (Python) on github.')
 
 st.subheader('A bit about me:')
-st.write('I am currently as second year Mathematics and Artificial Intelligence student as the University of Leicester, and my ambition for the future is a career in tech.')                          
+st.write('I am currently a second year Mathematics and Artificial Intelligence student as the University of Leicester, and my ambition for the future is a career in tech.')                          
